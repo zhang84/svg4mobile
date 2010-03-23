d11f112e31cbf4df8d0b40e19e2eb8cc9300c102
torrico1411 <torrico1411@5e6da53e-26e9-11df-be0e-258e15093178>
2010-03-23 23:24:17 +0000

    Nuevas clases Line y Rect

--- trunk/svg4mobile/src/com/grub/svg4mobile/DrawView.java
+++ trunk/svg4mobile/src/com/grub/svg4mobile/DrawView.java
@@ -1,60 +1,31 @@
 package com.grub.svg4mobile;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Paint;
-import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.View.OnTouchListener;
 
-public class DrawView extends View implements OnTouchListener {
-    private static final String TAG = "DrawView";
+public class DrawView extends View{
 
-    List<Point> points = new ArrayList<Point>();
-    Paint paint = new Paint();
-
+	private Line myLine;
+    private Rect myRect;
+    
     public DrawView(Context context) {
         super(context);
         setFocusable(true);
         setFocusableInTouchMode(true);
-
-        this.setOnTouchListener(this);
-
-        paint.setColor(Color.WHITE);
-        paint.setAntiAlias(true);
+         
+        myLine = new Line(10, 20, 30, 40, Color.WHITE);
+        
+        myRect = new Rect(100, 100, 200, 200, Color.CYAN);
+        
     }
 
     @Override
     public void onDraw(Canvas canvas) {
-        for (Point point : points) {
-            canvas.drawCircle(point.x, point.y, 5, paint);
-            // Log.d(TAG, "Painting: "+point);
-        }
+    	myLine.Draw(canvas);
+    	myRect.Draw(canvas);
+    	
     }
-
-    public boolean onTouch(View view, MotionEvent event) {
-        // if(event.getAction() != MotionEvent.ACTION_DOWN)
-        // return super.onTouchEvent(event);
-        Point point = new Point();
-        point.x = event.getX();
-        point.y = event.getY();
-        points.add(point);
-        invalidate();
-        Log.d(TAG, "point: " + point);
-        return true;
-    }
-}
-
-class Point {
-    float x, y;
-
-    @Override
-    public String toString() {
-        return x + ", " + y;
-    }
+    
 }
