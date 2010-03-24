35def21662ff8ee6ba5a34ccafdd8966757022f8
tanarro <tanarro@5e6da53e-26e9-11df-be0e-258e15093178>
2010-03-24 12:29:19 +0000

    Añado features que teniamos en OpenGL.

--- svg4mobile/src/com/grub/svg4mobile/Svg4mobile.java
+++ svg4mobile/src/com/grub/svg4mobile/Svg4mobile.java
@@ -1,27 +1,181 @@
 package com.grub.svg4mobile;
 
 import android.app.Activity;
-import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.DashPathEffect;
-import android.graphics.Paint;
-import android.graphics.Path;
-import android.graphics.Rect;
-import android.graphics.Camera;
 import android.os.Bundle;
-import android.view.View;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.Window;
+import android.view.WindowManager;
 
+/**
+ * 
+ * @author Daniel Lahoz
+ * @author Daniel Rivera
+ * @author Álvaro Tanarro
+ * @author Luis Torrrico
+ * @version 0.1
+ */
 public class Svg4mobile extends Activity {
+	
 	Svg4mobileView view;
+	private Menu menu;
+	private float xtemp, ytemp;
+	private float prevx, prevy;
+	private float x_mouse = 0, y_mouse =0;
+
 	
-	
-	/** Called when the activity is first created. */
+    /**
+     * Se ejecuta al iniciar la aplicación.
+     */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		view = new Svg4mobileView(this);
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE); 
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+        					 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		this.view = new Svg4mobileView(this);
 		setContentView(view);
 	}
+	
+	/**
+     * Se ejecuta cuando se crea el menu
+     * @param menu
+     * @return Devuelve true
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+       //call the base class to include system menus
+       super.onCreateOptionsMenu(menu);
+       this.menu = menu;
+       MenuItem itemChPersp = this.menu.add(0,1,0,"Perspectiva"); //Cambiar Perspectiva
+       itemChPersp.setIcon(android.R.drawable.ic_menu_mapmode);
+       
+       MenuItem itemZoomOut = this.menu.add(1,2,0,"Zoom -"); //Zoom -
+       MenuItem itemZoomIn = this.menu.add(1,3,1,"Zoom +"); //Zoom +
+       itemZoomOut.setIcon(android.R.drawable.btn_minus);
+       itemZoomIn.setIcon(android.R.drawable.btn_plus);
+       
+       MenuItem compassSync = this.menu.add(2,4,0,"Nortear"); //Nortear
+       MenuItem autoCompassSync = this.menu.add(2,5,1,"Autonortear"); //Autonortear
+       compassSync.setIcon(android.R.drawable.ic_menu_rotate);
+       autoCompassSync.setIcon(android.R.drawable.ic_menu_compass);
+       return true;
+    }
+   /**
+    * Manejador de teclas
+    * @param keyCode Código de tecla
+    * @param event Evento de tecla
+    * @return Devuelve true
+    */
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+      switch (keyCode) {
+        case KeyEvent.KEYCODE_DPAD_CENTER: {
+          // Resetea la posición de la cámara
+        	this.view.camReset();
+          break;
+        }        
+        case KeyEvent.KEYCODE_DPAD_LEFT: {
+          // Mueve la cámara hacia la derecha
+        	this.view.camLeft();
+          break;
+        }
+        case KeyEvent.KEYCODE_DPAD_RIGHT: {
+          // Mueve la cámara hacia la izquierda
+          this.view.getRight();
+          break;
+        }
+        case KeyEvent.KEYCODE_DPAD_UP: {
+          // Mueve la cámara hacia abajo
+          //this.renderer.zoomOut();
+        	this.view.camUp();
+          break;
+        }
+        case KeyEvent.KEYCODE_DPAD_DOWN: {
+        	// Mueve la cámara hacia arriba
+        	//this.renderer.zoomIn();
+        	this.view.camDown();
+        	break;
+        }
+        case KeyEvent.KEYCODE_Z: {
+        	// Aleja la cámara
+            this.view.zoomOut();
+          	
+            break;
+          }
+          case KeyEvent.KEYCODE_X: {
+          	// Acerca la cámara
+          	this.view.zoomIn();
+          	break;
+          }
+        case KeyEvent.KEYCODE_BACK: {
+        	// Sale del programa
+        	finish();
+        }
+        default: {
+        	return super.onKeyUp(keyCode, event);
+        }
+      }
+      return true;
+    }
+    
+    /**
+     * Manejador de los botones del menu
+     * @param item Elemento del menu
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) 
+    {
+       if (item.getItemId() == 1) {
+    	   
+       }
+       else if (item.getItemId() == 2) {
+    	   this.view.zoomOut();
+       }
+       else if (item.getItemId() == 3) {
+    	   this.view.zoomIn();
+       }
+       return true;
+    }
+
+    /**
+     * Maneja los eventos táctiles.
+     * @param event Evento
+     * @return Devuelve true si se ha producido algún evento táctil.
+     */
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+    	int action = event.getAction();
+    	switch (action) {
+               
+        case MotionEvent.ACTION_DOWN:
+        	xtemp = event.getX();
+        	ytemp = event.getY();
+        	x_mouse = xtemp;
+        	y_mouse = ytemp;
+            return true;
+
+        case MotionEvent.ACTION_MOVE:
+        	prevx = x_mouse;
+        	prevy = y_mouse;
+        	
+        	x_mouse = event.getX();
+        	if (xtemp < x_mouse && x_mouse > prevx)    	  
+      		  this.view.camLeft();
+      	  	else
+      		  this.view.getRight();
+        	y_mouse = event.getY();
+        	if (ytemp < y_mouse && y_mouse > prevy)    	  
+        	  this.view.camUp();
+        	else
+        	  this.view.camDown();
+        	return true;
+        }
+
+    	return true;
+    }
 
 }
